0e5a0aba59d672deb3d80654fa5734e4a784a933
ViswanathBizzukallu <Bizzukallub.24.beai@acharya.ac.in>
2025-05-24 15:30:18 +0530

    CollatzSequence.py

---
+++ CollatzSequence.py
@@ -0,0 +1,14 @@
+def collatz(number):
+    if number % 2 == 0:
+        result = number // 2
+    else:
+        result = 3 * number + 1
+    print(result)
+    return result
+
+try:
+    user_input = int(input("Enter an integer: "))
+    while user_input != 1:
+        user_input = collatz(user_input)
+except ValueError:
+    print("Please enter a valid integer.")
